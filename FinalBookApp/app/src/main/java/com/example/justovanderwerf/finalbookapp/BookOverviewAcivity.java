8fc217ac99dc768011e975de88ea8857c0c79fc1
Justo van der Werf <justovdwerf@gmail.com>
2018-01-19 15:38:47 +0100

    API deels werkend

--- FinalBookApp/app/src/main/java/com/example/justovanderwerf/finalbookapp/BookOverviewAcivity.java
+++ FinalBookApp/app/src/main/java/com/example/justovanderwerf/finalbookapp/BookOverviewAcivity.java
@@ -1,17 +1,122 @@
 package com.example.justovanderwerf.finalbookapp;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class BookOverviewAcivity extends AppCompatActivity {
+
+    ListView bookListView;
+    ArrayAdapter adapter;
+    RequestQueue queue;
+    List<String> list = new ArrayList<>();
+    List<String> idList = new ArrayList<>();
+    EditText searchBar;
+    String url = "https://www.googleapis.com/books/v1/volumes?maxResults=40&q=";
+    String newUrl;
+    String query;
+    private FirebaseAuth mAuth;
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_book_overview_acivity);
+
+
+        mAuth = FirebaseAuth.getInstance();
+        bookListView = findViewById(R.id.bookListView);
+        searchBar = findViewById(R.id.editTextBookTitle);
+        queue = Volley.newRequestQueue(this);
+
     }
+
+    // Make the query
+    public void searchOnClick(View view) {
+        query = searchBar.getText().toString();
+        newUrl = url+query;
+
+
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, newUrl,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        Log.d("RESPONSE", "onResponse: " + response);
+
+                        parseJSONResponse(response);
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.d("SEARCH", "SearchActivity: wrong");
+            }
+        });
+            queue.add(stringRequest);
+    }
+
+    private void parseJSONResponse(String response) {
+        try {
+            JSONObject obj = new JSONObject(response);
+            JSONArray arr = obj.getJSONArray("items");
+
+            list = new ArrayList<>();
+            idList = new ArrayList<>();
+
+            for(int i = 0; i < arr.length(); i++) {
+                JSONObject volume = arr.getJSONObject(i);
+                String title = volume.getJSONObject("volumeInfo").getString("title");
+                Log.d("RESPONSE", "title: " + title);
+                String id = volume.getString("id");
+                Log.d("RESPONSE", "id: " + id);
+                list.add(title);
+                idList.add(id);
+            }
+
+            updateListView();
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+//        setListener();
+        updateListView();
+    }
+
+    private void updateListView() {
+        adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, list);
+
+        bookListView.setAdapter(adapter);
+    }
+
+
 
     public void homeOnClick(View view) {
         Intent intent = new Intent(this, BookOverviewAcivity.class);
@@ -27,4 +132,6 @@ public class BookOverviewAcivity extends AppCompatActivity {
         Intent intent = new Intent(this, FriendsOverviewActivity.class);
         startActivity(intent);
     }
+
+
 }
--- FinalBookApp/app/src/main/java/com/example/justovanderwerf/finalbookapp/loginActivity.java
+++ FinalBookApp/app/src/main/java/com/example/justovanderwerf/finalbookapp/loginActivity.java
@@ -1,25 +1,122 @@
 package com.example.justovanderwerf.finalbookapp;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class loginActivity extends AppCompatActivity {
+
+    EditText usernameEdittext, passwordEdittext, emailEdittext;
+    Button loginButton, registerButton;
+    String email, password, username;
+    FirebaseDatabase database;
+    DatabaseReference users;
+
+    private FirebaseAuth mAuth;
+
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        database = FirebaseDatabase.getInstance();
+        users = database.getReference("users");
+
+        usernameEdittext = findViewById(R.id.usernameEditText);
+        emailEdittext = findViewById(R.id.emailEditText);
+        passwordEdittext = findViewById(R.id.passwordEditText);
+
+        loginButton = findViewById(R.id.buttonLogin);
+        registerButton = findViewById(R.id.buttonRegister);
+
+
     }
 
-    public void loginOnClick(View view) {
-        Intent intent = new Intent(this, BookOverviewAcivity.class);
-        startActivity(intent);
+    public void createUser(View view){
+        email = emailEdittext.getText().toString();
+        password = passwordEdittext.getText().toString();
+        username = usernameEdittext.getText().toString();
+
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d("succeslogin", "createUserWithEmail:success");
+                            String mail = email.replace('.',',');
+                            String uid = mAuth.getCurrentUser().getUid();
+                            DatabaseReference ref = database.getReference();
+                            if (username != ""){
+                                ref.child("users").child(username).child(username).setValue(username);
+                            }
+
+                            //ref.child("users").child(username).child(email).setValue(email); // dit doet het niet.
+
+                            Intent intent = new Intent(loginActivity.this, BookOverviewAcivity.class);
+                            startActivity(intent);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w("faillogin", "createUserWithEmail:failure", task.getException());
+                            Toast.makeText(loginActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+
+                        // ...
+                    }
+                });
     }
 
-    public void registerOnClick(View view) {
-        Intent intent = new Intent(this, BookOverviewAcivity.class);
-        startActivity(intent);
+
+
+
+    public void LogIn(View view) {
+        email = emailEdittext.getText().toString();
+        password = passwordEdittext.getText().toString();
+        username = usernameEdittext.getText().toString();
+
+
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d("succes", "signInWithEmail:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            Intent intent = new Intent(loginActivity.this, BookOverviewAcivity.class);
+                            startActivity(intent);
+
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w("failed", "signInWithEmail:failure", task.getException());
+                            Toast.makeText(loginActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+
+                        }
+
+                        // ...
+                    }
+                });
     }
 }
